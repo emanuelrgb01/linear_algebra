0247cbca10a22c19e4e2f343dbe2b1a90a5cc838
Emanuel Reinaldo <80110729+emanuelrgb01@users.noreply.github.com>
2023-03-14 17:45:25 -0300

    feat: Algorithm for Ax=b, A is upper triangular
    Algorithm for Ax=b, when A is upper triangular matrix.

---
+++ sub_inv.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def sub_inv(A, b):
+    for line in range(len(A)-1, -1, -1):
+        if abs(A[line][line]) < 1e-12:
+            print("Error")
+            return
+        S = 0
+        for column in range(line+1, len(A)-1, 1):
+            S = S + A[line][column]*b[column]
+        b[line] = (b[line] - S)/A[line][line]
+
+    print(b)
+
+
+if __name__ == '__main__':
+    A = np.array([[2, 1, 1],
+                  [0, 1, 1],
+                  [0, 0, 2]], np.float64)
+
+    b = np.array([[1],
+                  [0],
+                  [0]], np.float64)
+
+    sub_inv(A, b)
